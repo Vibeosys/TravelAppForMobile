8ed13150d7753976978fc32a097c5c49db8df046
maheshgiri <mahesh@vibeosys.com>
2015-10-07 11:56:54 +0530

    Updated
    On 07-10-2015

---
+++ TravelApp2/app/src/main/java/com/example/mahesh/travelapp/QuestionsFromOthers.java
@@ -0,0 +1,19 @@
+package com.example.mahesh.travelapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+/**
+ * Created by mahesh on 10/7/2015.
+ */
+public class QuestionsFromOthers extends AppCompatActivity {
+ListView questionslistView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otherquestionlist_layout);
+
+    }
+
+    }
