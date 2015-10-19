1bcfe3d102d939caf08bb2e01fc73772f6275f37
maheshgiri <mahesh@vibeosys.com>
2015-10-19 20:52:54 +0530

    Added Gallery View for Photos Added Map Markers Shows Saved Routes on Map Modified Questions and Answers
    Added Sliding Question Answers for Users.
    Added Line between markers
    Added Images in Database
    
    Added Destination Comments
    Added Photos Likes
    Added Database Integration at Startup of App
    
    Integration of Database with Service
    Added Asks Question and Answers for User.
    Modified Sqlite Database
    
    Integration of Send Messages with Database
    Integration of Users Messages with Database
    
    Integration of Comments and Likes with Database
    Showing Photos from Destination

---
+++ TravelApp2/app/src/main/java/com/vibeosys/travelapp/DestinationUsersImages.java
@@ -0,0 +1,28 @@
+package com.vibeosys.travelapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+import com.vibeosys.travelapp.util.Utils;
+
+/**
+ * Created by mahesh on 10/19/2015.
+ */
+public class DestinationUsersImages extends FragmentActivity{
+    private static final String TAG = "ImageGridActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            Utils.enableStrictMode();
+        }
+        super.onCreate(savedInstanceState);
+
+        if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
+            final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(android.R.id.content, new ImageGridFragment(), TAG);
+            ft.commit();
+        }
+    }
+}
